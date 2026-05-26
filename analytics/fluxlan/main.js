2df6341fdc6716ef554177662608815c30965da6
tromoSM <tromosm7@gmail.com>
2026-05-27 01:35:26 +0530

    added open dashboard redirection port to pass down to welcome page

--- analytics/fluxlan/main.js
+++ analytics/fluxlan/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded',function(){
-if(localStorage.getItem('last-fill')){window.location.replace('https://tromosm.github.io/FluxLAN-welcome-page/')}
+const info=new URLSearchParams(window.location.search);
+if(localStorage.getItem('last-fill')){window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)}
 const SUPABASE_URL="https://unalxdcdbgrqvveetxsn.supabase.co"
 const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuYWx4ZGNkYmdycXZ2ZWV0eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MDkyNTUsImV4cCI6MjA4NjE4NTI1NX0.8o5IZ_bs-8HaX70sP-e5_OUQfm5jyzXP6XMII1BoGSw"
 let tempdocs='tempdoc'
@@ -7,9 +8,8 @@ window.supabase=supabase.createClient(
 SUPABASE_URL,
 SUPABASE_ANON_KEY
 )
-const info=new URLSearchParams(window.location.search);
 if(!info.get('v')){
-    window.location.replace('https://tromosm.github.io/FluxLAN-welcome-page/')
+    window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)
 }
 (async()=>{
  const {error}=await supabase
@@ -19,4 +19,4 @@ if(!info.get('v')){
  if(error){
   console.log(error)
  }
- else{localStorage.setItem('last-fill',tempdocs);window.location.replace('https://tromosm.github.io/FluxLAN-welcome-page/')}})()})
+ else{localStorage.setItem('last-fill',tempdocs);window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)}})()})
