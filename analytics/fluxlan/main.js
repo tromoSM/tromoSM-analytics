468815866793b5c662f1f5ffd95beef0df970e36
tromoSM <tromosm7@gmail.com>
2026-06-14 20:50:48 +0530

    fix issue with localstorage

--- analytics/fluxlan/main.js
+++ analytics/fluxlan/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded',function(){
 const info=new URLSearchParams(window.location.search);
 if(localStorage.getItem('last-fill')){window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)}
+else{
 const SUPABASE_URL="https://unalxdcdbgrqvveetxsn.supabase.co"
 const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuYWx4ZGNkYmdycXZ2ZWV0eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MDkyNTUsImV4cCI6MjA4NjE4NTI1NX0.8o5IZ_bs-8HaX70sP-e5_OUQfm5jyzXP6XMII1BoGSw"
 let tempdocs='tempdoc'
@@ -19,4 +20,4 @@ if(!info.get('v')){
  if(error){
   console.log(error)
  }
- else{localStorage.setItem('last-fill',tempdocs);window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)}})()})
+ else{localStorage.setItem('last-fill',tempdocs);window.location.replace(`https://tromosm.github.io/FluxLAN-welcome-page/?r=${info.get('o')}`)}})()}})
